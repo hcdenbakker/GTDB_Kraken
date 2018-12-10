7f87cbe4a18e6d7228b004dfc0d82f494ab38b98
Lee Katz - Aspen <gzu2@cdc.gov>
2018-12-10 11:16:52 -0500

    heavy comments

--- tests/01_Salmonella.sh
+++ tests/01_Salmonella.sh
@@ -8,21 +8,26 @@ tmpdir=$(mktemp --directory ./$wd/gtdb_kraken.XXXXXX)
 trap ' { rm -rf $tmpdir; } ' EXIT
 echo "Temp dir is $tmpdir"
 
+# Reduce the input spreadsheet to simply the Salmonella genomes
 inputTsv="$tmpdir/Salmonella.tsv";
 grep g__Salmonella $wd/../data/gtdb.2018-12-10.tsv > $inputTsv || {
   echo "ERROR: no Salmonella was found in $inputTsv";
   exit 1
 }
+# Download the Salmonella genomes and create the taxonomy file
 perl $wd/../scripts/gtdbToTaxonomy.pl --infile $inputTsv
-ls -lhS
+# Move these files to temporary space so that they get cleaned up
 mv taxonomy library $tmpdir/
 
-# Build the database
+## Kraken build
 db=$wd/GTDB_Kraken
+# Add assemblies
 for i in $tmpdir/library/gtdb/*.fna; do
   kraken-build --add-to-library $i --db $db
 done
+# Add taxonomy
 mv -v $tmpdir/taxonomy $db
+# Format the database for Kraken
 kraken-build --build --db $db --threads $NUMCPUS
 
 # TODO test against the database
